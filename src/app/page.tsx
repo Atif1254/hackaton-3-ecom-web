dd0c287da6c79d73746ca38a9869144cf7fb7801
arsalan4work <you@example.com>
2025-01-22 20:42:09 +0500

    Error Handling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,13 @@
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import HomePage from "@/app/(store)/home/MainHome"
-import AddressForm from "@/components/Form";
 
 export default function Home() {
   return (
     <>
     <Header />
     <HomePage />
-    <AddressForm />
+
     <Footer />
     </>
   );
