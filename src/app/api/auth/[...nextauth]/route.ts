8b298ef1f8b3c35120bfb09932fba3d3099dcb74
arsalan4work <you@example.com>
2025-01-22 20:34:47 +0500

    Error handling

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,6 @@
-// src/pages/api/auth/[...nextauth].ts
 import NextAuth from "next-auth";
-import { authOptions } from "../../../../lib/Auth/auth";
+import { authOptions } from "@/lib/Auth/auth";
 
-export default NextAuth(authOptions);
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
